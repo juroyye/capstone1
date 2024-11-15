f40086a91b594d62a1a4882bb4773447a44101e4
Justin M <manningjustin08@yahoo.com>
2024-11-15 18:39:08 -0500

    added basic routing for home and login pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: '/home',
+    element: <Home/>,
+  },
+  {
+    path: '/login',
+    element: <Login/>,
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-    <Home/>
+     <RouterProvider router={router} />
     </div>
   );
 }

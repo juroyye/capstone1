789eb49acc3ffab3ada1a80ae7004c2027c7d69f
Justin M <manningjustin08@yahoo.com>
2024-12-09 16:49:39 -0500

    refactored bodyDiv

--- src/pages/Landing/BodyDiv.jsx
+++ src/pages/Landing/BodyDiv.jsx
@@ -1,19 +1,18 @@
-import React from 'react'
-import '../Landing/BodyDiv.css'
-import demoImg from '../../imports/images/demo-img-capstone.jpg'
-
+import React from 'react';
+import './BodyDiv.css';
+import demoImg from '../../imports/images/demo-img-capstone.jpg';
 
 const BodyDiv = () => {
   return (
-    <div class = 'bodyDiv'>
-        <div>
-          <img src={demoImg} alt='demo img'/>
-        </div>
-        <div>
-
-        </div>
+    <div className="bodyDiv">
+      <div className="image-div">
+        <img className="image" src={demoImg} alt="demo img" />
+      </div>
+      <div className="second-div">
+      
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default BodyDiv
+export default BodyDiv;

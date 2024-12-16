4baa037eed6e93603baa83e1c3f94de1fb7bb8af
Justin <manningjustin08@yahoo.com>
2024-12-16 00:49:08 -0500

    added stock instantiation

--- backend/src/main/java/com/stockproj/stockbackend/model/Stock.java
+++ backend/src/main/java/com/stockproj/stockbackend/model/Stock.java
@@ -1,4 +1,32 @@
 package com.stockproj.stockbackend.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
 public class Stock {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String symbol;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Double price;
+
+    @Column(nullable = false)
+    private LocalDateTime lastUpdated;
+
 }

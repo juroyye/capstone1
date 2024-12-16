dcfcb2072fdaaa7a6f4869173bb08c46194c97dc
Justin <manningjustin08@yahoo.com>
2024-12-15 22:27:43 -0500

    added backend folders and framing

---
+++ backend/src/main/java/com/stockproj/stockbackend/StockbackendApplication.java
@@ -0,0 +1,13 @@
+package com.stockproj.stockbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockbackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/stockproj/stockbackend/StockbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockproj.stockbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

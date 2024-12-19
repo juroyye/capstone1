d2bb622bf1578d19aaf8e0aa85b0a54de0f39e5d
Justin M <manningjustin08@yahoo.com>
2024-12-19 12:17:27 -0500

    added carousel functionality

--- src/pages/Landing/BodyDiv.jsx
+++ src/pages/Landing/BodyDiv.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import './BodyDiv.css';
 import demoImg from '../../imports/images/demo-img-capstone.jpg';
+import { Carousel } from 'react-bootstrap';
 
 const BodyDiv = () => {
   return (
     <div className="bodyDiv">
       <div className="image-div">
-        <img className="image" src={demoImg} alt="demo img" />
+        <Carousel>
+          <Carousel.Item>
+            <img className="d-block w-100" src={demoImg} alt="First slide" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className="d-block w-100" src={demoImg} alt="Second slide" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className="d-block w-100" src={demoImg} alt="Third slide" />
+          </Carousel.Item>
+        </Carousel>
       </div>
       <div className="description-div">
         <h2>Welcome to the leading stock information platform</h2>
